a7a49eddda43a07382d9365c2989e369818f25e3
waqil52 <waqilshopnil@gmail.com>
2019-08-07 21:47:10 +0600

    Home & About Page and Single Listing Page Done

--- realtors/models.py
+++ realtors/models.py
@@ -10,6 +10,7 @@ class Realtor(models.Model):
     email = models.CharField(max_length=50)
     is_mvp = models.BooleanField(default= False)
     hire_date = models.DateTimeField(default=datetime.now,blank=True)
+    objects = models.Manager()
     def __str__(self):
         return self.name
 
