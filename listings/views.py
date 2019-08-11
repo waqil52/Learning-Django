372e6aa034dd88fdb5396d9805e9adec891d04a7
waqil52 <waqilshopnil@gmail.com>
2019-08-12 01:14:15 +0600

    Filtered Search and Tweaks included

--- listings/views.py
+++ listings/views.py
@@ -31,9 +31,53 @@ def listing(request, listing_id):
 
 def search (request):
 
+    queryset_list = Listing.objects.order_by('-list_date')
+    #Keywords here 
+
+    if 'keywords' in  request.GET:
+        keywords = request.GET['keywords']
+
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains= keywords) 
+    
+    #Cities Here
+
+    if 'city' in  request.GET:
+        city = request.GET['city']
+
+        if city:
+            queryset_list = queryset_list.filter(city__iexact= city)
+
+    #states Here
+
+    if 'state' in  request.GET:
+        state = request.GET['state']
+
+        if state:
+            queryset_list = queryset_list.filter(state__iexact= state)
+
+    #bedrooms Here
+
+    if 'bedrooms' in  request.GET:
+        bedrooms = request.GET['bedrooms']
+
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte= bedrooms)
+
+
+    #bedrooms Here
+
+    if 'price' in  request.GET:
+        price = request.GET['price']
+
+        if price:
+            queryset_list = queryset_list.filter(price__lte= price)        
     context = {
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
-        'price_choices': price_choices
+        'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': request.GET
+
     }
     return render(request,'listings/search.html',context) 
